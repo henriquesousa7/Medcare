ac769e2430655760dd267b87a4c4f87021679939
hsantos <hsantos@clig.net.br>
2021-01-03 13:54:25 -0300

    add AlterarServidorAcolhimento usecase

--- src/main/java/br/edu/ifsp/application/main/Main.java
+++ src/main/java/br/edu/ifsp/application/main/Main.java
@@ -3,11 +3,13 @@ package br.edu.ifsp.application.main;
 import br.edu.ifsp.application.repository.InMemoryAcolhimentoDAO;
 import br.edu.ifsp.domain.entities.Acolhimento;
 import br.edu.ifsp.domain.usecases.Acolhimento.AcolhimentoDAO;
+import br.edu.ifsp.domain.usecases.Acolhimento.AlterarServidorAcolhimentoUC;
 import br.edu.ifsp.domain.usecases.Acolhimento.CadastrarServidorAcolhimentoUC;
 
 public class Main {
 
     private static CadastrarServidorAcolhimentoUC cadastrarServidorAcolhimentoUC;
+    private static AlterarServidorAcolhimentoUC alterarServidorAcolhimentoUC;
 
     public static void main(String[] args) {
         configureInjection();
@@ -15,13 +17,21 @@ public class Main {
         Acolhimento acolhimento1 = new Acolhimento("SC3009165", "Henrique de Sousa", "22222222222");
         Acolhimento acolhimento2 = new Acolhimento("SC300902X", "Felipe de Sousa", "11111111111");
 
+        System.out.println("Cadastrando");
         cadastrarServidorAcolhimentoUC.cadastraServidor(acolhimento1);
         cadastrarServidorAcolhimentoUC.cadastraServidor(acolhimento2);
+        System.out.println("OK");
+
+        acolhimento1.setNome("Diego Souza");
+        System.out.println("Atualizando");
+        alterarServidorAcolhimentoUC.alteraServidor(acolhimento1);
+        System.out.println("OK");
 
     }
 
     private static void configureInjection() {
         AcolhimentoDAO acolhimentoDAO = new InMemoryAcolhimentoDAO();
         cadastrarServidorAcolhimentoUC = new CadastrarServidorAcolhimentoUC(acolhimentoDAO);
+        alterarServidorAcolhimentoUC = new AlterarServidorAcolhimentoUC(acolhimentoDAO);
     }
 }
---
+++ src/main/java/br/edu/ifsp/domain/usecases/Acolhimento/AlterarServidorAcolhimentoUC.java
@@ -0,0 +1,22 @@
+package br.edu.ifsp.domain.usecases.Acolhimento;
+
+import br.edu.ifsp.domain.entities.Acolhimento;
+import br.edu.ifsp.domain.usecases.utils.EntityAlreadyExistsException;
+import br.edu.ifsp.domain.usecases.utils.EntityNotFoundException;
+
+public class AlterarServidorAcolhimentoUC {
+    private AcolhimentoDAO acolhimentoDAO;
+
+    public AlterarServidorAcolhimentoUC(AcolhimentoDAO acolhimentoDAO) {
+        this.acolhimentoDAO = acolhimentoDAO;
+    }
+
+    public boolean alteraServidor(Acolhimento acolhimento){
+        String prontuario = acolhimento.getProntuario();
+
+        if (acolhimentoDAO.findOne(prontuario).isEmpty())
+            throw new EntityNotFoundException("Servidor acolhimento nao encontrado");
+
+        return acolhimentoDAO.update(acolhimento);
+    }
+}
